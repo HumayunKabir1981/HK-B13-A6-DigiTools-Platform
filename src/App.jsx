1169539702c0cb3ef6d058492856b6795de8af66
HumayunKabir1981 <callmilton.com>
2026-03-30 20:26:56 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+     <h1 className='text-3xl'>Get started</h1>
+     <button className='btn btn-primary'>Buton Clicked</button>
+    </>
+  )
+}
+
+export default App
